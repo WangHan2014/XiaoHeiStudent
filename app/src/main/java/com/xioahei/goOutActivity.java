04401af1d08fc88d4d064f40ae2f5064f7e2a12b
WangHan2014 <53888453+WangHan2014@users.noreply.github.com>
2021-11-27 23:45:16 +0800

    完善退出按钮逻辑

--- app/src/main/java/com/xioahei/goOutActivity.java
+++ app/src/main/java/com/xioahei/goOutActivity.java
@@ -1,5 +1,7 @@
 package com.xioahei;
 
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -7,12 +9,16 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class goOutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_goOut);
+        setContentView(R.layout.activity_goout);
 
         StatusBarUtils.setWindowStatusBarColor(this, R.color.xiaobei_white);
         WebView broswer = findViewById(R.id.xiaoheiwebview);
@@ -24,7 +30,6 @@ public class goOutActivity extends AppCompatActivity {
 //        Toast.makeText(getApplicationContext(),args,Toast.LENGTH_LONG).show();
         broswer.getSettings().setJavaScriptEnabled(true);
         broswer.setWebViewClient(new WebViewClient() {
-
             /**
              * @param view
              * @param url
@@ -32,10 +37,44 @@ public class goOutActivity extends AppCompatActivity {
              */
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                //返回true
-                return true;
+                if (Build.VERSION.SDK_INT < 26) {
+                    view.loadUrl(url);
+                    return true;
+                }
+                return false;
             }
         });
+    }
+
+    /**
+     * Called when the activity has detected the user's press of the back
+     * key. The {@link #getOnBackPressedDispatcher() OnBackPressedDispatcher} will be given a
+     * chance to handle the back button before the default behavior of
+     * {@link Activity#onBackPressed()} is invoked.
+     *
+     * @see #getOnBackPressedDispatcher()
+     */
+    private static boolean mBackKeyPressed;
+
+    @Override
+    public void onBackPressed() {
+        WebView broswer = findViewById(R.id.xiaoheiwebview);
+        if (broswer.canGoBack()) {
+            User user = User.getInstance();
+            String args = "No=" + user.No + "&Name=" + user.Name + "&Class=" + user.Class + "&College=" + user.College + "&School=" + user.School;
+            broswer.loadUrl("file:///android_asset/before.html?" + args);
+        }
+        if (!mBackKeyPressed) {
+            Toast.makeText(this, "再按一次退出", Toast.LENGTH_SHORT).show();
+            mBackKeyPressed = true;
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    mBackKeyPressed = false;
+                }
+            }, 2000);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
